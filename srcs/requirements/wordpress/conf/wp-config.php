248afc3581296009405a79268a81df7d60eaeb5f
BH267 <hassanbenydir22@gmail.com>
2026-02-03 15:30:45 +0000

    update: get the env vars with getenv()

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,12 +1,12 @@
 <?php
-$db_password = trim(file_get_contents($MYSQL_PASSWORD_FILE));
-$wp_security_key = trim(file_get_contents($WP_SECURITY_KEYS_FILE));
+$db_password = trim(file_get_contents(getenv('MYSQL_PASSWORD_FILE')));
+$wp_security_key = trim(file_get_contents(getenv('WP_SECURITY_KEYS_FILE')));
 
 // Database settings
-define('DB_NAME', $MYSQL_DATABASE);
-define('DB_USER', $MYSQL_USER);
+define('DB_NAME', getenv('MYSQL_DATABASE'));
+define('DB_USER', getenv('MYSQL_USER'));
 define('DB_PASSWORD', $db_password);
-define('DB_HOST', $MYSQL_HOST);
+define('DB_HOST', getenv('MYSQL_HOST'));
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 define('WP_DEBUG', true);
@@ -20,18 +20,18 @@ define('WP_REDIS_DATABASE', 0);
 define('WP_CACHE', true);
 
 // Security keys
-define('AUTH_KEY',        $wp_security_key);
-define('SECURE_AUTH_KEY', $wp_security_key);
-define('LOGGED_IN_KEY',   $wp_security_key);
-define('NONCE_KEY',       $wp_security_key);
-define('AUTH_SALT',       $wp_security_key);
-define('SECURE_AUTH_SALT',$wp_security_key);
-define('LOGGED_IN_SALT',  $wp_security_key);
-define('NONCE_SALT',      $wp_security_key);
+define('AUTH_KEY',         $wp_security_key);
+define('SECURE_AUTH_KEY',  $wp_security_key);
+define('LOGGED_IN_KEY',    $wp_security_key);
+define('NONCE_KEY',        $wp_security_key);
+define('AUTH_SALT',        $wp_security_key);
+define('SECURE_AUTH_SALT', $wp_security_key);
+define('LOGGED_IN_SALT',   $wp_security_key);
+define('NONCE_SALT',       $wp_security_key);
 
-define( 'FTP_USER', $MYSQL_USER  );
-define( 'FTP_PASS', $db_password );
-define( 'FTP_HOST', $HOST_IP );
+define('FTP_USER', getenv('MYSQL_USER'));
+define('FTP_PASS', $db_password);
+define('FTP_HOST', getenv('HOST_IP'));
 
 $table_prefix = 'wp_';
 
@@ -39,6 +39,5 @@ if (!defined('ABSPATH')) {
     define('ABSPATH', __DIR__ . '/');
 }
 
-// Load WordPress
 require_once ABSPATH . 'wp-settings.php';
 ?>
